80d07727560fcba93850cd568e8ca49e3eeb2907
viktoria.gamayunova <viktoria.gamayunova@rgand.com>
2017-10-03 16:56:10 +0300

    fix mistake.

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,50 @@
 var lexus = {};
 var ferrari = {};
-function  fadeImages(images){
-    images.style.display="inline-block";
-}
-function ride(){
-    var images = document.getElementById("ride");
-    fadeImages(images);
-}
-function play(){
-    var images = document.getElementById("song");
-    fadeImages(images);
-}
-function rename(name) {
-    this.wheels = name;
-    console.log(this);
-}
 lexus = {
-    playMusic: play,
-    ride: ride,
     wheels: 'summer wheels',
-    changeWheels: rename
+    playMusic: function () {
+        console.log('бла бла')
+    },
+    ride: function () {
+        console.log('дыр дыр')
+    },
+    changeWheels: function () {
+        this.wheels = 'winter wheels';
+        console.log(this.wheels);
+    }
 }
 ferrari = {
-    playMusic: play,
-    ride:ride,
     wheels: 'winter wheels',
-    changeWheels: rename
+    playMusic:  function () {
+        console.log('бла бла')
+    },
+    ride: function () {
+        console.log('дыр дыр')
+    },
+    changeWheels: function () {
+        this.wheels ='summer wheels';
+        console.log(this.wheels);
+    }
 }
-// lexus.ride()
-// lexus.changeWheels()
-// lexus.playMusic();
-// ferrari.changeWheels('sprin');
+
+// function  fadeImages(images){
+//     images.style.display="inline-block";
+// }
+// function ride(){
+//     var images = document.getElementById("ride");
+//     fadeImages(images);
+// }
+// function play(){
+//     var images = document.getElementById("song");
+//     fadeImages(images);
+// }
+// function rename(name) {
+//     this.wheels = name;
+//     console.log(this);
+// }
+
+
+lexus.ride()
+lexus.changeWheels()
+lexus.playMusic();
+ferrari.changeWheels();
