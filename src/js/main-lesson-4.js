3e0a798c5778a19b25a1b9a35327994c2591ea6a
viktoria <viktoriagamaunova@gmail.com>
2017-10-08 22:20:51 +0300

    add plugin for searching string in array

--- src/js/main-lesson-4.js
+++ src/js/main-lesson-4.js
@@ -1,13 +1,39 @@
-
+/*
+*  Plugin for searching element in array
+*/
 (function () {
-    var array = [1,5,4,6,8854,5,8,4];
-    var lastPosition = array.length;
-    var findItem = 8;
-    for(var i = 0; i<lastPosition; i++){
-        var arrayValueItem = array[i];
-        if(arrayValueItem == findItem){
+    var haystack = [1,5,4,6,8854,5,8,4];
+    var needle = 8;
+    for(var i = 0; i<haystack.length; i++){
+        var arrayValueItem = haystack[i];
+        if(arrayValueItem == needle){
             console.log(arrayValueItem);
             break
         }
     }
 }());
+
+/*
+*  Plugin for order array
+*/
+(function () {
+    var array = [1,2,3,4,5,6];
+    var newArray = [];
+
+}());
+
+/*
+*  Plugin for searching string in array
+*/
+(function () {
+    var haystack = [1,2,3,'Test',5,6];
+    var needle = 'string';
+    for(var i=0; i < haystack.length; i++){
+        var arrayValueItem = haystack[i];
+       if( typeof(arrayValueItem) === needle ){
+            console.log('this string is ' + arrayValueItem)
+       }
+    }
+}());
+
+
