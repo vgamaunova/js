718f46c5aa08aef3e05ef401e75cccfd7c110eb8
viktoria <viktoriagamaunova@gmail.com>
2017-10-11 10:11:02 +0300

    remove variable

--- src/js/main-lesson-4.js
+++ src/js/main-lesson-4.js
@@ -5,9 +5,8 @@
     var haystack = [1, 5, 4, 6, 8854, 5, 8, 4];
     var needle = 8;
     for (var i = 0; i < haystack.length; i++) {
-        var arrayValueItem = haystack[i];
-        if (arrayValueItem == needle) {
-            console.log(arrayValueItem);
+        if ( haystack[i] == needle) {
+            console.log( haystack[i]);
             break;
         }
     }
