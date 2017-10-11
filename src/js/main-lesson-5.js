d03b9a8d21e5c8982c561bc45f3ab70d7a7d8897
viktoria <viktoriagamaunova@gmail.com>
2017-10-11 19:22:46 +0300

    add functional for game

---
+++ src/js/main-lesson-5.js
@@ -0,0 +1,29 @@
+function computerProgress() {
+    var numberCol = Math.floor(Math.random() * 9) + 1;
+    var itemColArray = document.querySelectorAll('.col:nth-child('+numberCol+')');
+    var  itemCol = itemColArray[0];
+    if(document.querySelectorAll('.disabled').length <=8){
+        if(!itemCol.classList.contains('disabled')){
+            itemCol.classList.add('disabled');
+            itemCol.innerText = 'x';
+            return false;
+        }else{
+            computerProgress();
+        }
+    }else{
+        document.getElementById('message').innerText = 'Game over';
+        return false;
+    }
+}
+function userProgress(param) {
+    if(document.querySelectorAll('.disabled').length <= 8){
+        if(!param.classList.contains('disabled')){
+            param.classList.add('disabled');
+            param.innerText = '0';
+            return false;
+        }
+    }else{
+        document.getElementById('message').innerText = 'Game over';
+        return false;
+    }
+}
