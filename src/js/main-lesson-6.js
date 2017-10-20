9247d63ee10228de735434cd99a4939154aad32b
viktoria.gamayunova <viktoria.gamayunova@rgand.com>
2017-10-20 18:31:11 +0300

    add function time

--- src/js/main-lesson-6.js
+++ src/js/main-lesson-6.js
@@ -12,12 +12,14 @@ function fib(n) {
 console.log( fib(3) ); // 2
 console.log( fib(7) ); //13
 
+var positionTop = 0;
 // (function(){
-//     var top = 0;
-//     function positionElement() {
-//         var element =  document.getElementById('song');
-//         element.style="position: absolute;top:"+top+"";
-//         return top = top + 10;
-//     }
-//    setInterval(positionElement, 100);
+    function positionElement() {
+        var element =  document.getElementById('song');
+        positionTop  = positionTop + 10;
+        console.log(positionTop);
+    }
 // }())
+setInterval(positionElement, 2000);
+
+// setInterval(console.log(function(){return top = top + 10}), 2000);
