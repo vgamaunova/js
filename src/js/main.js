bdf1ac167d36a59407f6ef6b9c64053d84373fb5
viktoria.gamayunova <viktoria.gamayunova@rgand.com>
2017-11-03 10:49:32 +0200

    add car

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,23 @@
 function car(name){
     this.name = name;
-    this.speed = 0;
+};
+car.prototype.go = function() {
+    console.log( 'go ' + this.name );
 };
 
-var cabriolet = {
-    jumps: true,
-    __proto__: car
+function  cabriolet(name) {
+    this.name = name;
 };
+cabriolet.prototype = Object.create(car.prototype);
+
+
+cabriolet.prototype.go = function() {
+    console.log( 'hinged roof and  go');
+};
+
+var cabrioletCar = new cabriolet('cabriolet');
+
+var truck =  new car('DAF');
+
+cabrioletCar.go();
+truck.go();
